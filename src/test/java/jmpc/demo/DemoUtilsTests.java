d2643a923c5e10fbbd3c17b3e9baa39b7b1f9b44
juanmiguel431 <juanmiguel431@gmail.com>
2024-11-10 22:15:23 -0400

    DemoUtilsTests - testNotNull

--- src/test/java/jmpc/demo/DemoUtilsTests.java
+++ src/test/java/jmpc/demo/DemoUtilsTests.java
@@ -1,16 +1,28 @@
 package jmpc.demo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DemoUtilsTests {
 
+    private DemoUtils demoUtils;
+
+    @BeforeEach
+    public void setup() {
+        demoUtils = new DemoUtils();
+    }
+
     @Test
     public void testEqualsAndNotEquals() {
-        var demoUtils = new DemoUtils();
 
         Assertions.assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
     }
 
+    @Test
+    public void testNotNull() {
+
+        Assertions.assertNotNull(demoUtils.checkNull(5));
+    }
 
 }
