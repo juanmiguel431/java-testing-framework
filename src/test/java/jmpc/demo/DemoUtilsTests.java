f6f9404294fc79b73c7ba54f25f4f4078c967e0f
juanmiguel431 <juanmiguel431@gmail.com>
2024-11-17 20:00:20 -0400

    Assertions - Array Equals.

--- src/test/java/jmpc/demo/DemoUtilsTests.java
+++ src/test/java/jmpc/demo/DemoUtilsTests.java
@@ -46,13 +46,9 @@ public class DemoUtilsTests {
     @Test
     @DisplayName("Same and Not Same - String literals")
     public void testSameAndNotSameV1() {
-
         // Both values will reference the same objet.
         var firstName1 = "Juan";
         var firstName2 = "Juan";
-
-        var lastName1 = new String("Paulino");
-        var lastName2 = new String("Paulino");
 
         Assertions.assertSame(firstName1, firstName2, "Objects should refer to the same objects");
         Assertions.assertNotSame(5, 6, "Values must be different.");
@@ -65,5 +61,14 @@ public class DemoUtilsTests {
         var lastName2 = new String("Paulino");
 
         Assertions.assertNotSame(lastName1, lastName2, "Values must be different.");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    public void testArrayEquals() {
+        String[] items = { "A", "B", "C" };
+        var demo = demoUtils.getFirstThreeLettersOfAlphabet();
+
+        Assertions.assertArrayEquals(items, demo, "Arrays should be equals");
     }
 }
